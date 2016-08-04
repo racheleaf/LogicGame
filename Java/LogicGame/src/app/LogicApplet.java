c3b68c835a6127b9edb8d49bcec249ea61319649
Brice Huang <bmhuang@mit.edu>
2016-08-03 22:16:15 -0400

    OH MY GOD THIS WORKS
    GUI can receive/display stuff from server now! (still lots of jankiness - cleanup is a mess and the applet doesn't shut down correctly, and right now the applet doesn't do any GUI stuff beyond printing what the server says.  but IT WORKS YAY)

--- Java/LogicGame/src/app/LogicApplet.java
+++ Java/LogicGame/src/app/LogicApplet.java
@@ -26,7 +26,8 @@ public class LogicApplet extends Applet implements ActionListener{
     private BufferedReader in;
     private PrintWriter out;
 
-    private TextField input, output;
+    private TextField input;
+    private TextField output1, output2, output3, output4, output5, output6, output7, output8, output9, output10;
 
 //    private LogicFrame logicframe;
     
@@ -63,22 +64,49 @@ public class LogicApplet extends Applet implements ActionListener{
             this.serverSocket = connectToLogicServer();
             this.in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
             this.out = new PrintWriter(serverSocket.getOutputStream(), true);
-            //serverSocket.setSoTimeout(0); <-- THIS IS SUPER JANK 
+            serverSocket.setSoTimeout(0); //<-- THIS IS SUPER JANK 
         } catch (IOException ioe) {
             ioe.printStackTrace();
             throw new RuntimeException();
         }
         // Construct the TextFields
         this.input = new TextField(40);
-        this.output = new TextField(40);
-        this.output.setEditable(false);
+        this.output1 = new TextField(40);
+        this.output2 = new TextField(40);
+        this.output3 = new TextField(40);
+        this.output4 = new TextField(40);
+        this.output5 = new TextField(40);
+        this.output6 = new TextField(40);
+        this.output7 = new TextField(40);
+        this.output8 = new TextField(40);
+        this.output9 = new TextField(40);
+        this.output10 = new TextField(40);
+        this.output1.setEditable(false);
+        this.output2.setEditable(false);
+        this.output3.setEditable(false);
+        this.output4.setEditable(false);
+        this.output5.setEditable(false);
+        this.output6.setEditable(false);
+        this.output7.setEditable(false);
+        this.output8.setEditable(false);
+        this.output9.setEditable(false);
+        this.output10.setEditable(false);
         Button b = new Button("Enter");
         b.addActionListener(this);
         
         // add elements
         this.add(input);
         this.add(b);
-        this.add(output);
+        this.add(output1);
+        this.add(output2);
+        this.add(output3);
+        this.add(output4);
+        this.add(output5);
+        this.add(output6);
+        this.add(output7);
+        this.add(output8);
+        this.add(output9);
+        this.add(output10);
 
         // sets size of applet
         setSize(APPLET_WIDTH, APPLET_HEIGHT);
@@ -87,16 +115,32 @@ public class LogicApplet extends Applet implements ActionListener{
 
     @Override
     public void start() {
-        // for some reason this doesn't work
-//        try{
-//            for (String line = in.readLine(); line!= null; 
-//                    line = in.readLine()){
-//                output.setText(line);
-//            }
-//        } catch (IOException ioe){
-//            ioe.printStackTrace();
-//            throw new RuntimeException();
-//        }
+        // a loop that listens for stuff from server and 
+        // writes it to the applet
+        new Thread(new Runnable(){
+            public void run(){
+                try{
+                    for (String line = in.readLine(); line!= null; 
+                            line = in.readLine()){
+                        //System.out.println(line);
+                        output10.setText(output9.getText());
+                        output9.setText(output8.getText());
+                        output8.setText(output7.getText());
+                        output7.setText(output6.getText());
+                        output6.setText(output5.getText());
+                        output5.setText(output4.getText());
+                        output4.setText(output3.getText());
+                        output3.setText(output2.getText());
+                        output2.setText(output1.getText());
+                        output1.setText(line);
+                    }
+                } catch (IOException ioe){
+                    ioe.printStackTrace();
+                    throw new RuntimeException();
+                }
+                
+            }
+        }).start();
     }
     
     @Override
